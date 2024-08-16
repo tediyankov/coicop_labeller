8f87829175d9d172408dfa1c42a0daf4f718341f
Daniela Shuman <danielashuman@Danielas-MacBook-Pro.local>
2024-08-16 15:08:41 -0500

    lintr cleaned up code

--- coicop_labeller.R
+++ coicop_labeller.R
@@ -1,44 +1,44 @@
-pacman::p_load(dplyr, 
-               fuzzyjoin, 
+pacman::p_load(dplyr,
+               fuzzyjoin,
                e1071,
                openai,
                stringr,
-               worlddataverse, 
-               tm, 
+               worlddataverse,
+               tm,
                fastNaiveBayes,
-               tidytext, 
-               SnowballC, 
-               forcats, 
-               ggplot2, 
-               WeightSVM, 
-               caret, 
-               superml, 
-               text2vec, 
+               tidytext,
+               SnowballC,
+               forcats,
+               ggplot2,
+               WeightSVM,
+               caret,
+               superml,
+               text2vec,
                tidyr)
 
-options(warn=-1)
+options(warn = -1)
 
 source("labels.R")
 source("config.R")
 
-args <- commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly = TRUE)
 
 err_missing_args <- paste0("Missing parameters provided. This function requires three arguments. \n",
-              "\tfirst argument should be a string specifying the path to the products CSV\n",
-              "\tsecond argument should be a string with the name of the product id column\n",
-              "\tthird argument should be a string with the name of the product name column\n",
-              "try:\n",
-              "\tRscript coicop_labeller.R sample.csv index product_name_en")
+                           "\tfirst argument should be a string specifying the path to the products CSV\n",
+                           "\tsecond argument should be a string with the name of the product id column\n",
+                           "\tthird argument should be a string with the name of the product name column\n",
+                           "try:\n",
+                           "\tRscript coicop_labeller.R sample.csv index product_name_en")
 
 # Checking for arguments
-if (length(args) < 3){
+if (length(args) < 3) {
   cat(err_missing_args)
   quit()
-} else if (length(args) == 3){
+} else if (length(args) == 3) {
   product_path <- args[1]
   product_id_col_name <- args[2]
-  product_col_name <- args[3] 
-} else if (length(args) == 4){
+  product_col_name <- args[3]
+} else if (length(args) == 4) {
   product_path <- args[1]
   product_id_col_name <- args[2]
   product_col_name <- args[3]
@@ -61,79 +61,86 @@ if (!grepl("\\.csv$", product_path)) {
 if (file.exists(product_path)) {
   products <- read.csv(product_path)
   missing_columns <- setdiff(c(product_id_col_name, product_col_name), colnames(products))
-  
+
   if (length(missing_columns) > 0) {
-    cat(paste("The following columns are missing from the products CSV:", paste(missing_columns, collapse = ", ")))
+    cat(paste("The following columns are missing from the products CSV:",
+              paste(missing_columns, collapse = ", ")))
     quit()
   }
-  
+
 } else {
   # File does not exist
   cat(paste("The file at", product_path, "does not exist."))
   quit()
 }
 
-if (is.na(gpt_output_file) || gpt_output_file == ''){
+if (is.na(gpt_output_file) || gpt_output_file == "") {
   position <- regexpr("\\.csv$", product_path)
   gpt_output_file <- paste0(substr(product_path, 1, position - 1), "_output.csv")
-  
-  if (verbose){print(paste("Missing GPT Output File. Setting GPT Output File name to", gpt_output_file))}
+
+  if (verbose) {
+    print(paste("Missing GPT Output File. Setting GPT Output File name to",
+                gpt_output_file))
+  }
 }
 
 Sys.setenv(
   OPENAI_API_KEY = OPEN_API_KEY
 )
 
-# This function takes in a csv of products that contains the following columns: 
+# This function takes in a csv of products that contains the following columns:
 #   product_id_col_name: unique per row, this can simply be the index
 #   product_col_name: the name of the product attempting to be classified
 
 coicop_labeller <- function(products,
-                product_id_col_name,
-                product_col_name
-                ){
-  
-  execution_times_path <- paste(local_path, "execution_times_per_100k_", OPEN_AI_MODEL, ".csv", sep="")
-  
+                            product_id_col_name,
+                            product_col_name
+) {
+
   get_file_name <- function(chunk_index) {
     # Make the file name based on the date of when the labels were created
-    return(paste("gen_labels_", MODEL_SET, "_", chunk_index, sep=""))
+    return(paste("gen_labels_", MODEL_SET, "_", chunk_index, sep = ""))
   }
-  
+
   generate_labels <- function(chunk_index, products_list) {
     prompt <- paste("Return the coicop codes for the items below in a csv format with the product_id and the coicop label such as: \n product_id, coicop label. ",
                     "Classify all ", CHUNK_SIZE, " of the labels provided below.",
-                    chunk_to_str(products_list[[chunk_index]]),sep="")
-    out_file_name <- paste(local_path, get_file_name(chunk_index), ".txt", sep="")
-    
+                    chunk_to_str(products_list[[chunk_index]]), sep = "")
+    out_file_name <- paste(local_path, get_file_name(chunk_index), ".txt", sep = "")
+
     response <- create_chat_completion(
       model = OPEN_AI_MODEL,
       temperature = .5,
       n = 1,
-      messages=list(
-        list("role" = "system", 
+      messages = list(
+        list("role" = "system",
              "content" = systeminput),
-        list("role" = "user", 
+        list("role" = "user",
              "content" = prompt)
       )
     )
     writeLines(response$choices$message.content, out_file_name)
-    cost = as.numeric(response$usage$prompt_tokens)*0.005 / 1000 + 
-      as.numeric(response$usage$completion_tokens)*0.0025 / 1000
-    if (verbose){print(paste("Full Tokens used: ", response$usage$total_tokens, " Approximate Cost: $", round(cost, digits=2), sep=""))}
+    cost <- as.numeric(response$usage$prompt_tokens) * 0.005 / 1000 +
+      as.numeric(response$usage$completion_tokens) * 0.0025 / 1000
+    if (verbose) {
+      print(paste("Full Tokens used: ", 
+                  response$usage$total_tokens,
+                  " Approximate Cost: $",
+                  round(cost, digits = 2), sep = ""))
+    }
   }
-  
+
   predictions_to_numeric <- function(db) {
     db$coicop_modeled_1 <- as.numeric(db$coicop_modeled_1)
     db$coicop_modeled_2 <- as.numeric(db$coicop_modeled_2)
     db$coicop_modeled_3 <- as.numeric(db$coicop_modeled_3)
     return(db)
   }
-  
+
   # Convert to a CSV
   gen_labels_to_csv <- function(chunk_index) {
-    out_file_name <- paste(local_path, get_file_name(chunk_index), ".txt", sep="")
-    
+    out_file_name <- paste(local_path, get_file_name(chunk_index), ".txt", sep = "")
+
     # Removes any lines that don't follow the product code format
     lines <- readLines(out_file_name)
     csv_line <- grep("csv", lines)
@@ -143,18 +150,18 @@ coicop_labeller <- function(products,
 
     txt <- read.csv(temp_file, header = TRUE, stringsAsFactors = FALSE)
     # Removes Whitespace
-    txt[] <- lapply(txt, function(x) if(is.character(x)) trimws(x) else x)
+    txt[] <- lapply(txt, function(x) if (is.character(x)) trimws(x) else x)
     colnames(txt) <- c(product_id_col_name, "coicop.label")
     code_pattern <- "^\\d{1,2}\\.\\d{1}\\.\\d{1}\\.\\d{1}$"
-    
+
     labelled_by_index <- txt %>%
-      filter(str_detect(coicop.label, code_pattern) | 
-                  str_detect(coicop.label, "^\\d{1,2}\\.\\d{1}\\.\\d{1}$") | 
-                  str_detect(coicop.label, "^\\d{1,2}\\.\\d{1}$") | 
-                  str_detect(coicop.label, "^\\d{1,2}$")
-             ) %>%
+      filter(str_detect(coicop.label, code_pattern) |
+          str_detect(coicop.label, "^\\d{1,2}\\.\\d{1}\\.\\d{1}$") |
+          str_detect(coicop.label, "^\\d{1,2}\\.\\d{1}$") |
+          str_detect(coicop.label, "^\\d{1,2}$")
+      ) %>%
       rename(code = coicop.label)
-    
+
     labelled_by_index <- labelled_by_index %>%
       mutate(code = case_when(
         str_detect(code, "^\\d{1,2}\\.\\d{1}\\.\\d{1}$") ~ paste0(code, ".0"),
@@ -162,85 +169,102 @@ coicop_labeller <- function(products,
         str_detect(code, "^\\d{1,2}$") ~ paste0(code, ".0.0.0"),
         TRUE ~ code
       ))
-    
-    labelled_by_index <- separate(labelled_by_index, code, into = c("coicop_modeled_1", "coicop_modeled_2", 
+
+    labelled_by_index <- separate(labelled_by_index, code, into = c("coicop_modeled_1", "coicop_modeled_2",
                                                                     "coicop_modeled_3"),
                                   sep = "\\.", fill = "right", convert = TRUE)
     labelled_by_index <- predictions_to_numeric(labelled_by_index)
-    
+
     # add the product name
-    labelled_by_index <- labelled_by_index %>% 
+    labelled_by_index <- labelled_by_index %>%
       merge(select(products, c(product_col_name, product_id_col_name)), by = product_id_col_name, all.x = TRUE)
-    
-    write.csv(labelled_by_index, paste(local_path, get_file_name(chunk_index), ".csv", sep=""))
+
+    write.csv(labelled_by_index, paste(local_path, get_file_name(chunk_index), ".csv", sep = ""))
   }
-  
-  update_labelled_df <- function(chunk_index, execution_time=NA) {
-    newly_labelled_data <- read.csv(paste(local_path, get_file_name(chunk_index), ".csv", sep=""))
-    execution_time_per_100k <- as.numeric(execution_time) / nrow(newly_labelled_data) * 100000
-    shared_cols = c(product_id_col_name, product_col_name, "coicop_modeled_1", "coicop_modeled_2", "coicop_modeled_3")
-    
-    if (verbose){print(paste(nrow(newly_labelled_data), "labels were generated out of", CHUNK_SIZE))}
-    if(file.exists(gpt_output_file)) {
+
+  update_labelled_df <- function(chunk_index, execution_time = NA) {
+    newly_labelled_data <- read.csv(paste(local_path, get_file_name(chunk_index), ".csv", sep = ""))
+    shared_cols <- c(product_id_col_name, product_col_name, "coicop_modeled_1", "coicop_modeled_2", "coicop_modeled_3")
+
+    if (verbose) {
+      print(paste(nrow(newly_labelled_data), "labels were generated out of", CHUNK_SIZE))
+    }
+    if (file.exists(gpt_output_file)) {
       labelled_data <- read.csv(gpt_output_file)
-      
       labelled_data <- predictions_to_numeric(labelled_data)
       newly_labelled_data <- predictions_to_numeric(newly_labelled_data)
-      
-      # Add newly labelled rows to the existing set of labelled entries. 
-      newly_labelled_data <- rbind(labelled_data %>% select(all_of(shared_cols)), 
-                                   newly_labelled_data %>% select(all_of(shared_cols)))
+
+      # Add newly labelled rows to the existing set of labelled entries.
+      newly_labelled_data <- rbind(labelled_data %>%
+          select(all_of(shared_cols)),
+          newly_labelled_data %>%
+            select(all_of(shared_cols))
+      )
     }
-    write.csv(select(newly_labelled_data, shared_cols), gpt_output_file, row.names=FALSE)
+    write.csv(select(newly_labelled_data, shared_cols), gpt_output_file, row.names = FALSE)
   }
-  
+
   products_to_label <- products
-  
+
   # Checks if existing GPT output file exists
-  if(file.exists(gpt_output_file)) {
+  if (file.exists(gpt_output_file)) {
     # Only need this to remove rows that have already been labelled by GPT
-    if (verbose){print(paste("Existing full GPT output file exists at", gpt_output_file))}
-    labelled_data <- read.csv(gpt_output_file) %>% 
+    if (verbose) {
+      print(paste("Existing full GPT output file exists at", gpt_output_file))
+    }
+    labelled_data <- read.csv(gpt_output_file) %>%
       select(all_of(product_id_col_name))
-    
+
     # Only process the rows that contain product codes not in the existing file
-    products_to_label <- products[!(products[product_id_col_name] %in% labelled_data[product_id_col_name]), ] %>% 
+    products_to_label <- products[!(products[product_id_col_name] %in% labelled_data[product_id_col_name]), ] %>%
       select(all_of(c(product_id_col_name, product_col_name)))
-    
+
   } else {
-    if (verbose){
-      print(paste("No full GPT output file exists with the name: ", gpt_output_file, 
+    if (verbose) {
+      print(paste("No full GPT output file exists with the name: ", gpt_output_file,
                 ". Creating new file with this name and location.",
-                sep=""))
+                sep = ""))
     }
   }
-  
-  if (verbose){print(paste("Out of", nrow(products), "products,", nrow(products_to_label), "labels will be generated. If the numbers differ, then there may have been some products that have already been labelled."))}
-  
-  chunk_to_str <- function(chunk){
+
+  if (verbose) {
+    print(paste("Out of", nrow(products), "products,", nrow(products_to_label),
+                "labels will be generated. If the numbers differ, then there may
+                have been some products that have already been labelled."))
+  }
+
+  chunk_to_str <- function(chunk) {
     output_string <- apply(chunk, 1, function(row) {
       paste0(row[product_id_col_name], " ", row[product_col_name])
     })
     return(paste(output_string, collapse = "\n"))
   }
-  
+
   # Batch into labels to maximize GPT output
   products_to_label_chunked <- products_to_label %>%
     mutate(group_index = ceiling(row_number() / CHUNK_SIZE))
-  products_to_label_chunked <- split(products_to_label_chunked, products_to_label_chunked$group_index)
-  
+  products_to_label_chunked <- split(products_to_label_chunked,
+                                     products_to_label_chunked$group_index)
+
   for (chunk_index in names(products_to_label_chunked)) {
-    chunk <- products_to_label_chunked[[chunk_index]]
-    
-    if (verbose){print(paste("Generating Labels for Chunk", chunk_index, "of", length(products_to_label_chunked), "chunks of size", CHUNK_SIZE))}
-    execution_time <- system.time(generate_labels(chunk_index, products_to_label_chunked))
-    execution_time <- 0
+    if (verbose) {
+      print(paste("Generating Labels for Chunk",
+                  chunk_index, "of",
+                  length(products_to_label_chunked),
+                  "chunks of size", CHUNK_SIZE))
+    }
+    execution_time <- system.time(generate_labels(chunk_index,
+                                                  products_to_label_chunked))
     gen_labels_to_csv(chunk_index)
-    if (verbose){print(paste("Execution Time: ", execution_time['elapsed']))}
-    if (verbose){print(paste("Updating Files in existing DB for Chunk", chunk_index))}
-    update_labelled_df(chunk_index, execution_time=execution_time['elapsed'])
+    if (verbose) {
+      print(paste("Execution Time: ", execution_time["elapsed"]))
+    }
+    if (verbose) {
+      print(paste("Updating Files in existing DB for Chunk", chunk_index))
+    }
+    update_labelled_df(chunk_index, execution_time = execution_time["elapsed"])
   }
 }
 
 # Additional parameters can be set in the config.R file
-coicop_labeller(products, product_id_col_name, product_col_name) 
+coicop_labeller(products, product_id_col_name, product_col_name)
--- config.R
+++ config.R
@@ -1,10 +1,12 @@
-if (!exists("OPEN_API_KEY")){
-  if (!file.exists("keys.R")){
+if (!exists("OPEN_API_KEY")) {
+  if (!file.exists("keys.R")) {
     stop("No OPEN_API_KEY exists and no file keys.R exists.")
-  } else{
-    source('keys.R')
+  } else {
+    source("keys.R")
   }
-  if (!exists("OPEN_API_KEY") || (exists("OPEN_API_KEY") && OPEN_API_KEY == '')){
+  if (!exists("OPEN_API_KEY") ||
+        (exists("OPEN_API_KEY") &&
+           OPEN_API_KEY == "")) {
     stop("No OPEN_API_KEY exists in keys.R")
   }
 }
@@ -12,20 +14,21 @@ if (!exists("OPEN_API_KEY")){
 # -----------
 # GPT Function Defaults
 # -----------
-verbose=TRUE
+verbose <- TRUE
 
 systeminput <- paste("You are a classifier that needs to classify an inputted revenue stream description as one of many CoiCop labels. These CoiCop labels have 4 levels. For example, Food & Beverage is level 1, Food is level 2, Bread & Cereals is level 3, Cereals is level 4. The format of the label is x.x.x.x, with each x being a level. If it is a general enough category, input 0s for each of the following level labels. The following is a string containing the entire labelling system.", 
                      coicop_labels_str_stripped)
-CHUNK_SIZE = 500
+CHUNK_SIZE <- 500
 
-# the Open AI Model parameter needs to be in the Open AI Model format found here: https://platform.openai.com/docs/models/
-OPEN_AI_MODEL = "gpt-4o" 
+# the Open AI Model parameter needs to be in the Open AI Model
+# format found here: https://platform.openai.com/docs/models/
+OPEN_AI_MODEL <- "gpt-4o"
 
 # path and name of output file
-gpt_output_file = "" 
+gpt_output_file <- ""
 
 # path to folder where all GPT files will be uploaded
-local_path = "" 
+local_path <- ""
 
 # model can be anything that is a unique model
-MODEL_SET = Sys.Date() 
+MODEL_SET <- Sys.Date()
